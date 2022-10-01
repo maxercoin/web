83ca449afccc5d9dde757d61fa4aba49071aad40
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-01 18:16:15 +0100

    Update About.js

--- components/utils/About.js
+++ components/utils/About.js
@@ -1,17 +1,35 @@
-import { Box, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
 
 function About() {
   return (
-    <Box id='about' mt='5' display='grid' placeItems='center' width='95%' overflow='hidden'>
-        <Box w='95%'>
-            <Text textShadow='1px 2px #000' fontSize='1.5em' color='#fff' textAlign='center' fontWeight='bold'>ABOUT US</Text>
-            <Text fontSize='1em' color='#fff' textAlign='center'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry{"'"}s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </Text>
-        </Box>
+    <Box
+      id="about"
+      mt="5"
+      display="grid"
+      placeItems="center"
+      width="95%"
+      overflow="hidden"
+    >
+      <Box w="95%">
+        <Text
+          textShadow="1px 2px #000"
+          fontSize="1.5em"
+          color="#fff"
+          textAlign="center"
+          fontWeight="bold"
+        >
+          ABOUT US
+        </Text>
+        <Text fontSize="1em" color="#fff" textAlign="center">
+          Maxercoin helps make saving and investing seamless. Maxercoin take
+          away the stress and planning required to save and invest on a regular
+          basis through its automated and easy-to-use platform. Your savings
+          also generate interests until a set maturity date.
+        </Text>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default About
+export default About;
