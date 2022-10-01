93469b61a299c6ff6972284c7123f52066b258f4
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-01 21:24:53 +0100

    Update carousel.js

--- components/utils/carousel.js
+++ components/utils/carousel.js
@@ -1,35 +1,19 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import styles from "./carousel.module.css";
-import { AspectRatio, Box, Image as ChakraImg, Text } from "@chakra-ui/react";
+import { EffectCoverflow, Navigation, Pagination } from "swiper";
+import { Box, Image as ChakraImg, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import items from "./items";
-// import banner1 from "./slider/banner1.jpg";
-// import banner2 from "./slider/banner2.png";
-// import banner3 from "./slider/banner3.png";
-// import banner4 from "./slider/banner4.png";
-// import banner5 from "./slider/banner5.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import style from "./carousel.module.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/mousewheel";
+import "swiper/css/keyboard";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
 function Carouselcomponent() {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
   const carouselBoxes = {
     display: "grid",
     placeItems: "center",
@@ -47,60 +31,70 @@ function Carouselcomponent() {
     // zIndex:'1'
   };
   return (
-    <Carousel
-      itemClass={styles.items}
-      showDots
-      dotListClass={styles.dotStyles}
-      autoPlay
-      infinite
-      className={styles.carousel}
-      responsive={responsive}
-    >
-      {items.map((v, index) => (
-        <Box
-          key={`${index}carouselhome`}
-          {...carouselBoxes}
-          position="relative"
-          overflow="hidden"
-          width="100%"
-          h="100%"
-        >
-          <ChakraImg
-            className={styles.img}
-            as={Image}
-            alt="Banner1"
-            layout="intrinsic"
-            width="100vw"
-            src={v.img}
-          />
-          <Box {...carouselBoxestextsBox}>
-            <Text
-              color="#fff"
-              fontSize={["1.1em","1.2em", "1.5em", "2em"]}
-              fontWeight="500"
-              p="5"
-              wordWrap={true}
-              className="firstBanner"
-              textAlign="center"
+    <Box w="100vw" display="flex" justifyItems="center">
+      <Swiper
+        modules={[EffectCoverflow, Navigation, Pagination]}
+        spaceBetween={5}
+        slidesPerView={1}
+        navigation
+        keyboard
+        pagination={{ clickable: true }}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        className={style.swiper}
+      >
+        {items.map((v, index) => (
+          <SwiperSlide className={style.slide} key={index + "carousel"}>
+            <Box
+              key={`${index}carouselhome`}
+              {...carouselBoxes}
+              position="relative"
+              overflow="hidden"
+              width="100%"
+              h="100%"
             >
-              {v.header}
-            </Text>
-            <Text
-              flex={1}
-              wordWrap={true}
-              p="5"
-              textAlign="center"
-              color="#fff"
-              fontSize={["0.8em","1em","1.2em"]}
-              className="firstBanner"
-            >
-              {v.content}
-            </Text>
-          </Box>
-          <Box {...carouselboxOverLay}></Box>
-        </Box>
-      ))}
-    </Carousel>
+              <ChakraImg
+                className={styles.img}
+                as={Image}
+                alt="Banner1"
+                layout="intrinsic"
+                src={v.img}
+              />
+              <Box {...carouselBoxestextsBox}>
+                <Text
+                  color="#fff"
+                  fontSize={["1.1em", "1.2em", "1.5em", "2em"]}
+                  fontWeight="500"
+                  p="5"
+                  wordWrap={true}
+                  className="firstBanner"
+                  textAlign="center"
+                >
+                  {v.header}
+                </Text>
+                <Text
+                  flex={1}
+                  wordWrap={true}
+                  p="5"
+                  textAlign="center"
+                  color="#fff"
+                  fontSize={["0.8em", "1em", "1.2em"]}
+                  className="firstBanner"
+                >
+                  {v.content}
+                </Text>
+              </Box>
+              <Box {...carouselboxOverLay}></Box>
+            </Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
   );
 }
 
