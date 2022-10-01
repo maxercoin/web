8398ea9b6fda10fc22e9e12a5ae5452b79b8768b
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-01 18:17:04 +0100

    Update contact.js

--- components/utils/contact.js
+++ components/utils/contact.js
@@ -1,19 +1,59 @@
-import React from 'react'
-import {Box,Button,Input,Stack,Text, Textarea} from '@chakra-ui/react'
-import colors from './colors'
+import React from "react";
+import { Box, Button, Input, Stack, Text, Textarea } from "@chakra-ui/react";
+import colors from "./colors";
 function Contact() {
   return (
-    <Box id='contactus' mb='5' display='flex' flexDirection='column' alignItems='center' w='100%' mt='5'>
-        <Text color='#fff' fontWeight='bold' textAlign='center' textShadow='1px 2px #000' fontSize='1.5em'>CONTACT US</Text>
-        <Stack display='flex' padding={1} paddingBottom={3} flexDirection='column' alignItems='center'spacing={4} w='70%' mt='2' border='1px solid #fff' borderRadius='5px'>
-          
-        <Input type='text' w='100%' bg='#fff' placeholder='Input your name: ' />
-        <Input type='email' w='100%' bg='#fff' placeholder='Input your email: ' />
-        <Textarea bg='#fff' placeholder='Your Message' />
-        <Button _hover={{bg:colors.secondarycolor,color:'#fff'}} w='40%'>SUBMIT</Button>
-        </Stack>
+    <Box
+      id="contactus"
+      mb="5"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      w="100%"
+      mt="5"
+    >
+      <Text
+        color="#fff"
+        fontWeight="bold"
+        textAlign="center"
+        textShadow="1px 2px #000"
+        fontSize="1.5em"
+      >
+        CONTACT US
+      </Text>
+      <Text textAlign="center" color="#fff">
+        Don't hesitate to contact us for any kind of information
+      </Text>
+      <Text textAlign="center" color="#fff">
+        Your feedback helps us to improve our system and provide you with the
+        best investment experience tailored to your needs.
+      </Text>
+      <Stack
+        display="flex"
+        padding={1}
+        paddingBottom={3}
+        flexDirection="column"
+        alignItems="center"
+        spacing={4}
+        w="70%"
+        mt="2"
+        border="1px solid #fff"
+        borderRadius="5px"
+      >
+        <Input type="text" w="100%" bg="#fff" placeholder="Input your name: " />
+        <Input
+          type="email"
+          w="100%"
+          bg="#fff"
+          placeholder="Input your email: "
+        />
+        <Textarea bg="#fff" placeholder="Your Message" />
+        <Button _hover={{ bg: colors.secondarycolor, color: "#fff" }} w="40%">
+          SUBMIT
+        </Button>
+      </Stack>
     </Box>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
