8a62e825988b4ff6fe248b6da12414811a9dc6c9
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-03 22:49:06 +0100

    Update confirm.js

--- pages/api/user/auth/signup/confirm.js
+++ pages/api/user/auth/signup/confirm.js
@@ -1,35 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 /**
- * @param {import('next').NextApiRequest } req 
- * @param {import('next').NextApiResponse} res 
+ * @param {import('next').NextApiRequest } req
+ * @param {import('next').NextApiResponse} res
  */
-import bcrypt from 'bcrypt'
-import user from '../../../../../backendUtils/model/model';
-import connectDB from '../../../../../backendUtils/db/connetMongo';
-import { sendConfirmationEmail } from '../../../../../backendUtils/mailer/mail';
- export default async function handler(req, res) {
-    try{
-    await connectDB()
-    if(req.method?.toLocaleLowerCase() === "get"){
-
-        const {hash} = req.query;
-        if(!hash) res.status(404).send("Nothing to verify");
-        if(!(await user.findById(hash))) res.status(404).send("User does not exist");
-        const updateUser = user.findByIdAndUpdate(
-            hash,
-            {
-                $set:{activated:true}
-            },
-            (err,docs)=>{
-                if(err) res.status(400).send(err);
-                if(docs) res.status(200).send("Success!")
-            }
-            )
-            console.log(updateUser)
-        
+import bcrypt from "bcrypt";
+import user from "../../../../../backendUtils/model/model";
+import connectDB from "../../../../../backendUtils/db/connetMongo";
+import { sendConfirmationEmail } from "../../../../../backendUtils/mailer/mail";
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+    if (req.method?.toLocaleLowerCase() === "get") {
+      const { hash } = req.query;
+      if (!hash) res.status(404).send("Nothing to verify");
+      if (!(await user.findById(hash)))
+        res.status(404).send("User does not exist");
+      const updateUser = user.findByIdAndUpdate(
+        hash,
+        {
+          $set: { activated: true },
+        },
+        (err, docs) => {
+          if (err) res.status(400).send(err);
+          if (docs) res.redirect(307, "/login");
+        }
+      );
+      console.log(updateUser);
     }
-         
-    }catch(err){
-    res.status(400).send(err.message);    
-}
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 }
