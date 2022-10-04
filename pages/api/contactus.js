31aa5fa405fbd64194c5a02209a6b5f4533cbf88
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-04 08:39:31 +0100

    Create contactus.js

---
+++ pages/api/contactus.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/**
+ * @param {import('next').NextApiRequest } req
+ * @param {import('next').NextApiResponse} res
+ */
+
+import { sendContactUs } from "../../backendUtils/mailer/mail";
+
+export default async function handler(req, res) {
+  if (req.method?.toLocaleLowerCase() === "post") {
+    const { email, username, message } = req.body;
+    if (!email || !username || !message) {
+      return res.status(400).send("All field must be valid");
+    }
+    try {
+      await sendContactUs(username, email, message);
+      res.status(200).send("Message sent!");
+    } catch (e) {
+      console.log(e);
+      res.status(400).send("An error occurred");
+    }
+  }
+}
