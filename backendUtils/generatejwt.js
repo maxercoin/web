f5b82ebc0e11ca218557d69ce2aafd54db41fe86
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-08 22:25:46 +0100

    Update generatejwt.js

--- backendUtils/generatejwt.js
+++ backendUtils/generatejwt.js
@@ -1,23 +1,58 @@
+import JWT from "jsonwebtoken";
+import { sendConfirmationEmail } from "./mailer/mail";
+const generateCookieResponse = async (
+  statusCode,
+  res,
+  id,
+  role,
+  active = false,
+  walletAddress,
+  userdata,
+  mode
+) => {
+  const token = JWT.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 
-import JWT from 'jsonwebtoken'
-const generateCookieResponse = (statusCode, res, id, role,active=false,walletAddress,userdata) => {
-    const token = JWT.sign({ id, role }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
-    });
- 
-   return res.status(statusCode).json({
-      success: true,
-      token,
-      active,
-      id,walletAddress,userdata
-    });
-  };
-  
-  //  Generate JWT
-  const generateToken = (id, role) => {
-    return JWT.sign({ id, role }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
-    });
-  };
+  if (mode === "signup") {
+    const returnValue = (isSuccess) => {
+      if (isSuccess) {
+        return res.status(200).json({
+          success: true,
+          token,
+          active,
+          id,
+          walletAddress,
+          userdata,
+        });
+      } else {
+        return res.status(401).json({
+          success: false,
+          token,
+          active,
+          id,
+          walletAddress,
+          userdata,
+        });
+      }
+    };
+    return sendConfirmationEmail(toUser, id, "", returnValue);
+  }
+  return res.status(statusCode).json({
+    success: true,
+    token,
+    active,
+    id,
+    walletAddress,
+    userdata,
+  });
+};
 
-  export default generateCookieResponse;
+//  Generate JWT
+const generateToken = (id, role) => {
+  return JWT.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+export default generateCookieResponse;
