ea7a451b63b576efc8144adb8b2d0a893765f012
maxercoin <113921993+maxercoin@users.noreply.github.com>
2022-10-13 21:22:16 +0100

    Update body.js

--- components/body/body.js
+++ components/body/body.js
@@ -1,27 +1,43 @@
-import { Box,Image as ChakraImg,Text } from '@chakra-ui/react'
-import React,{useState} from 'react'
-import colors from '../utils/colors'
-import Image from 'next/image';
+import { AspectRatio, Box, Image as ChakraImg, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import colors from "../utils/colors";
+import Image from "next/image";
 
-import styles from './body.module.css';
-import Carouselcomponent from '../utils/carousel';
-import About from '../utils/About';
-import Faqs from '../utils/faqs';
-import Contact from '../utils/contact';
+import styles from "./body.module.css";
+import Carouselcomponent from "../utils/carousel";
+import About from "../utils/About";
+import Faqs from "../utils/faqs";
+import Contact from "../utils/contact";
+import VideoCarousel from "../utils/VideoCarousel";
 
-function Bodycomponent({faq}) {
-  
-    const {primarycolor,secondarycolor,deg1color,deg2color,deg3color,primaryTextColor} = colors;
-    
-    
+function Bodycomponent({ faq }) {
+  const {
+    primarycolor,
+    secondarycolor,
+    deg1color,
+    deg2color,
+    deg3color,
+    primaryTextColor,
+  } = colors;
+
   return (
-    <Box display='flex' flexDirection='column' alignItems='center' overflow='hidden' minH='90vh' w='100vw' bg={primarycolor} bgGradient={`linear(to-r,${primarycolor},${secondarycolor})`}>
-       <Carouselcomponent />
-       <About />
-       <Faqs faq={faq}/>
-       <Contact />
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      overflow="hidden"
+      minH="90vh"
+      w="100vw"
+      bg={primarycolor}
+      bgGradient={`linear(to-r,${primarycolor},${secondarycolor})`}
+    >
+      <Carouselcomponent />
+      <VideoCarousel />
+      <About />
+      <Faqs faq={faq} />
+      <Contact />
     </Box>
-  )
+  );
 }
 
-export default Bodycomponent
+export default Bodycomponent;
